4702460ffc91fc2e9e9d7e1e6618b455d25da1b3
Erik Lindqvist <eriksmejl@gmail.com>
2014-01-09 17:35:32 +0100

    Change module to not handle download of images anymore
    Separation of concerns. The module should not do this, it should be handled by the user in "Titanium-land".
    
    Note! This commit breaks backward compability!

--- example/app.js
+++ example/app.js
@@ -1,20 +1,33 @@
-// This is a test harness for your module
-// You should do something interesting in this harness 
-// to test out the module and to provide instructions 
-// to users on how to use it by example.
-
-
-// open a single window
+// Create Window and views
 var win = Ti.UI.createWindow({
 	backgroundColor:'white'
 });
 
 var avatar = Ti.UI.createImageView();
+var roundedAvatar = Ti.UI.createImageView({
+    left: 10
+});
+
+var container = Ti.UI.createView({
+    top: 10,
+    left: 10,
+    layout: 'horizontal'
+});
+
+// Initialize the module
 var ra = require('sc.roundedavatar');
 
-win.add(avatar);
+// Fetch an image and set it as the references ImageView's image
+avatar.image = 'http://graph.facebook.com/ronald.dev.3/picture?width=200&height=200';
 
-//Adding a 200x200 avatar and resizing it to 64x64
-avatar.image = ra.getRoundedAvatar("http://graph.facebook.com/zuck/picture?width=200&height=200", 64);
+// Create a rounded version of the image with the width and height of 64px
+// Note how the module requires you to the image as blob!
+var imageBlob = ra.getRoundedAvatar(avatar.toBlob(), 64);
 
+roundedAvatar.image = imageBlob;
+
+container.add(avatar);
+container.add(roundedAvatar);
+
+win.add(container);
 win.open();
