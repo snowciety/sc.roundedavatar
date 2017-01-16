a8424c9ad9ca468cb5ec8478e906b77ac0122805
Carlos Henrique Zinato <carlos@conference-compass.com>
2017-01-16 18:09:12 +0100

    Support for Titanium SDK 6.+

--- src/sc/roundedavatar/RoundedAvatarModule.java
+++ src/sc/roundedavatar/RoundedAvatarModule.java
@@ -22,7 +22,7 @@ import java.net.URL;
 
 import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
-import org.appcelerator.kroll.common.Log;
+//import org.appcelerator.kroll.common.Log;
 import org.appcelerator.titanium.TiApplication;
 import org.appcelerator.titanium.TiBlob;
 
@@ -37,37 +37,38 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.util.Log;
 
 @Kroll.module(name="RoundedAvatar", id="sc.roundedavatar")
 public class RoundedAvatarModule extends KrollModule {
-	
+
 	private final static String TAG = "RoundedAvatarModule";
-	
+
 	public RoundedAvatarModule(){
 		super();
 	}
-	
+
 	@Kroll.onAppCreate
 	public static void onAppCreate(TiApplication app)
 	{
 		// put module init code that needs to run when the application is created
 	}
-	
+
 	@Override
 	public void onResume(Activity activity) {
 		super.onResume(activity);
 	}
-	
+
 	@Override
 	public void onDestroy(Activity activity) {
 		super.onDestroy(activity);
 	}
-	
+
 	private static void debugMsg(String msg)	{
-		Log.debug(TAG, msg);
+		Log.d(TAG, msg);
 	}
-	
-	
+
+
 	/**
 	 * Returns a rounded bitmap from a bitmap passed as a blob from Titanium.
 	 * @param imageBlob	The image as a blob.
@@ -77,39 +78,41 @@ public class RoundedAvatarModule extends KrollModule {
 	@Kroll.method
 	public TiBlob getRoundedAvatar(TiBlob imageBlob, int width) {
 		if (width <= 0) {
-			throw new IllegalArgumentException("Parameter 'width' has to be greater than zero!");
+			Log.d(TAG, "Parameter 'width' has to be greater than zero!");
+			return imageBlob;
 		}
 		debugMsg("Received blob with size "+Integer.toString(imageBlob.getBytes().length));
 		Bitmap image = BitmapFactory.decodeByteArray(imageBlob.getBytes(), 0, imageBlob.getBytes().length);
 		if (image != null) {
 			return getRoundedBitmapAsBlob(resizeBitmap(image, width, width));
 		} else {
-			throw new IllegalArgumentException("The passed blob could not be converted to a bitmap!");
+			Log.d(TAG, "The passed blob could not be converted to a bitmap!");
+			return imageBlob;
 		}
 	}
-	
+
 	private static TiBlob getRoundedBitmapAsBlob(Bitmap bitmap) {
 	    final Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
 	    final Canvas canvas = new Canvas(output);
-	 
+
 	    final int color = Color.RED;
 	    final Paint paint = new Paint();
 	    final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
 	    final RectF rectF = new RectF(rect);
-	 
+
 	    paint.setAntiAlias(true);
 	    canvas.drawARGB(0, 0, 0, 0);
 	    paint.setColor(color);
 	    canvas.drawOval(rectF, paint);
-	 
+
 	    paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
 	    canvas.drawBitmap(bitmap, rect, rect, paint);
-	 
+
 	    bitmap.recycle();
-	 
+
 	    return TiBlob.blobFromImage(output);
 	  }
-	
+
 	/**
 	 * Resizes a bitmap to a specified size.
 	 * @param bitmap	The bitmap to resize.
